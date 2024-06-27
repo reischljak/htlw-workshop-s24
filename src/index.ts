8a5ff726de7c4dbfff796bd0d005506f4f4b1ca9
Jakob Reischl <jakob.reischl@htl-wels.at>
2024-06-27 10:33:21 +0200

    first commit

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import dotenv from "dotenv";
+import envUtil from "./util/env.util";
+
+dotenv.config();
+
+
+console.log(typeof envUtil.PORT);
---
+++ src/util/env.util.ts
@@ -0,0 +1,28 @@
+import { object, string, number, coerce } from "zod";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const envSchema = object({
+    //PORT: string({
+    //    message: "PORT must be defined!"
+    //}).transform(portStr => {
+    //    try {
+    //        const port = parseInt(portStr);
+//
+    //        if (port < 1 || port > 65535) {
+    //            throw "PORT not in valid range";
+    //        }
+//
+    //        return port;
+    //    } catch(e) {
+    //        console.error(e);
+    //    }
+    //})
+    PORT: coerce.number({
+        message: "PORT must be a number!"
+    }).min(0).max(65536)
+});
+
+
+export default envSchema.parse(process.env);
