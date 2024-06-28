8c7ef50528fa197794bbfb6303434f90e7a13ca0
Jakob Reischl <jakob.reischl@htl-wels.at>
2024-06-28 11:00:31 +0200

    feat: move files to separate backend folder

---
+++ backend/src/util/env.util.ts
@@ -0,0 +1,32 @@
+import { object, string, number, coerce } from "zod";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const envSchema = object({
+    //PORT: string({
+    //    message: "PORT must be defined!"
+    //}).transform(portStr => {
+    //    try {
+    //        const port = parseInt(portStr);
+//
+    //        if (port < 1 || port > 65535) {
+    //            throw "PORT not in valid range";
+    //        }
+//
+    //        return port;
+    //    } catch(e) {
+    //        console.error(e);
+    //    }
+    //})
+    PORT: coerce.number({
+        message: "PORT must be a number!"
+    }).min(0).max(65535),
+
+    MONGO_URL: string({
+        message: "required"
+    })
+});
+
+
+export default envSchema.parse(process.env);
